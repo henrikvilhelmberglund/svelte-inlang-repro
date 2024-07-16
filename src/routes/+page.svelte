171c7bf481edecb00e8fcc10074283896bedb29c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-07-16 16:48:58 +0200

    feat: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import * as m from "$lib/paraglide/messages.js";
+</script>
+
+<svelte:head>
+  <meta name="description" content="{m.title()}" />
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
